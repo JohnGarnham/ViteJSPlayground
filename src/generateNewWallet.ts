89c191e9a18b6fb865601e3b3fe2f3020e85abc2
JohnGarnham <john.e.garnham@gmail.com>
2021-06-26 12:36:49 -0600

    Add argument for # of addresses to generate

--- src/generateNewWallet.ts
+++ src/generateNewWallet.ts
@@ -1,9 +1,18 @@
+// Usage: $ generateNewWallet [number-of-addresses]
+// Generates a new wallet and either 5 or number-of-addresses addresses
+// And outputs information 
+
 // Import wallet module from vitejs
 // Wallet reference: https://docs.vite.org/vitej/modules/wallet.html#wallet
 import {wallet} from '@vite/vitejs';
 
 // Generate a brand new wallet
-console.log("Generating New Wallet")
+var examples = 5;
+if(process.argv[2] != undefined) {
+    examples = parseInt(process.argv[2]);
+}
+console.log("Examples: ", examples);
+console.log("Generating New Wallet and ")
 const myWallet = wallet.createWallet();
 console.log('RootPath: ', myWallet.rootPath);
 console.log('Mnemonics: ', myWallet.mnemonics);
@@ -12,9 +21,11 @@ console.log('Seed Hex: ', myWallet.seedHex);
 console.log('ID: ', myWallet.id);
 
 // Derive an address
-const addressObj = myWallet.deriveAddress(0);
-console.log('\nAddress: ', addressObj.address);
-console.log('Original Address: ', addressObj.originalAddress);
-console.log('Private Key: ', addressObj.privateKey);
-console.log('Public Key: ', addressObj.publicKey);
-console.log('Path: ', addressObj.path);
+for(var i = 0; i < examples; i++) {
+    const addressObj = myWallet.deriveAddress(i);
+    console.log('\nPath: ', addressObj.path);
+    console.log('Address: ', addressObj.address);
+    console.log('Original Address: ', addressObj.originalAddress);
+    console.log('Private Key: ', addressObj.privateKey);
+    console.log('Public Key: ', addressObj.publicKey);
+}
