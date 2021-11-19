fe728a7f6bc15fd54aaccfa187b16ad7fa54c863
JohnGarnham <john.e.garnham@gmail.com>
2021-11-18 20:18:58 -0700

    Add queryViteScan to make CSV of tti holders

--- src/queryVitescan.ts
+++ src/queryVitescan.ts
@@ -14,5 +14,24 @@ const url = "https://vitescan.io/vs-api/token?tokenId=" + tti;
 
 // Fetch URL. Print output
 fetch(url)
-.then(response => response.text())
-.then(data => console.log(data));
+.then(response => {
+    if (!response.ok) {
+        throw new Error("HTTP error " + response.status);
+    }
+    return response.json();
+})
+.then(json => {
+  // Output CSV file with holder data
+  let data = json.data;
+  console.log("Holders: " + data.holderCount);
+  // Headers
+  console.log("Address,Balance,Percentage,Tx Count")
+  // Output each account data as CSV string
+  for(var i = 0; i < data.accountsResults.length; i++) {
+    let account = data.accountsResults[i];
+    console.log([account.address,account.balance,account.percentage,account.txnCount].join(','));
+  }
+}) 
+.catch(function (error) {
+  console.log(error);
+})
